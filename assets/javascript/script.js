701485477193b9fef04ef4ad6c3733b6e532ac6a
edricwong28 <edricwong28@gmail.com>
2017-10-25 14:04:05 -0700

    button working, changed href to src in script tag

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,11 @@
 var answers = ['Yes','No','Maybe','Ask Again Later', 'Seems Unlikely','Did you really just wish for that?'];
 
-// document.getElementById('#button').onclick = function(){showAnswer()}
-
-// function showAnswer(){
-// 	var response = [Math.floor(Math.random)*answers.length];
-// 	div.innerHTML = response;
-// 	console.log(response);
-// };
-
-$('#askBtn').click(function(){
-	//$('#answer').html(answers[Math.floor(Math.random()*answers.length)];);
+$('#askBtn').on('click', function(){
+	$('#answer').html(answers[Math.floor(Math.random()*answers.length)]);
 	console.log('button working');
-	console.log(answers[Math.floor(Math.random()*answers.length)];);
+	console.log(answers[Math.floor(Math.random()*answers.length)]);
 });
+
+// $('#alert').click(function(){
+// 	alert("working");
+// })
